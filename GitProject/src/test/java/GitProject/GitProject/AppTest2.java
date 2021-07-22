3b8597e802d08a4803cfbd8e5fb960ea85f5b62a
Alok Kumar <alok.kumar@capgemini.com>
2021-07-22 20:08:29 +0530

    New File created

---
+++ GitProject/src/test/java/GitProject/GitProject/AppTest2.java
@@ -0,0 +1,15 @@
+package GitProject.GitProject;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest2  extends TestCase
+{
+	public static void main (String args[]){
+		System.out.println("Third Test Case in Git Project");
+	}
+}
