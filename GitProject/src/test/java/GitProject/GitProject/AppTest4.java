8eae18fff824a3a39542d64ee1aed52bd39c6427
Alok Kumar <alok.kumar@capgemini.com>
2021-07-22 20:38:13 +0530

    Added App4

---
+++ GitProject/src/test/java/GitProject/GitProject/AppTest4.java
@@ -0,0 +1,15 @@
+package GitProject.GitProject;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest4  extends TestCase
+{
+	public static void main (String args[]){
+		System.out.println("Fifth Test Case in Git Project");
+	}
+}
