6572544d2e367ad5df46105134ebd7c408e47b51
Alok Kumar <alok.kumar@capgemini.com>
2021-07-22 20:30:06 +0530

    Created App3

---
+++ GitProject/src/test/java/GitProject/GitProject/AppTest3.java
@@ -0,0 +1,15 @@
+package GitProject.GitProject;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest3  extends TestCase
+{
+	public static void main (String args[]){
+		System.out.println("Fourth Test Case in Git Project");
+	}
+}
